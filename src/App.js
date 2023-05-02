0069d5795ad290a66e7ccbfd7ce1b2b1bdcc2f5d
Arana27 <116308837+Arana27@users.noreply.github.com>
2023-05-02 01:14:24 -0600

    Componente 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./App.css";
+
+const Input = (props) => {
+
+  const { id, placeholder, name, label, type = "text" } = { ...props };
+  return (
+    <div className="col-lg-5" key={id}>
+      <label htmlFor={id} className="form-label">
+        {label}
+      </label>
+      <input
+        name={name}
+        type={type}
+        className="form-control"
+        id={id}
+        placeholder={placeholder}
+        required
+      />
+      <div className="valid-feedback">¡correcto! 😻</div>
+      <div className="invalid-feedback">Debe ingresar el campo 😻</div>
+    </div>
+  );
+};
 
 function App() {
   return (
