c39d70456772f37d62af5a7458aeb0d5db941eae
AdanAguilar69 <89608796+AdanAguilar69@users.noreply.github.com>
2023-05-02 03:04:46 -0600

    docpdf

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { jsPDF } from "jspdf";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./App.css";
@@ -29,21 +30,21 @@ const Input = (props) => {
 
 function App() {
   let links = ["facebook", "whatsapp", "Instagram", "Twitter", "TikTok"];
-  const [inputValue, setInputValue] = useState({correocarinosa:"", correoartistico:"", Numerocedula:"", estadocivil:"", parejas:"", discapacidad:"", edad:"", nivelacademico:"", direccion:"", habilidades:"",capacidad:"", correo:"", orientacionsexual:"", salario:"", hijos:"", fecha:""});
+  const [inputValue, setInputValue] = useState({nombrecarinosa:"",nombreartistico:"", Numerocedula:"", estadocivil:"", parejas:"", discapacidad:"", edad:"", nivelacademico:"", direccion:"", habilidades:"",capacidad:"", correo:"", orientacionsexual:"", salario:"", hijos:"", fecha:""});
 
   let inputsProps = [];
 
   inputsProps.push({
-    id: "correocarinosa",
-    placeholder: "Ingrese su correo",
+    id: "nombrecarinosa",
+    placeholder: "Ingrese su nombre",
     name: "correocarinosa",
     label: "Cual es su correo:",
     value: inputValue.correocarinosa
   });
   inputsProps.push({
-    id: "correoartistico",
-    placeholder: "Cual es su correo artistico",
-    name: "correoartistico",
+    id: "nombreartistico",
+    placeholder: "Cual es su nombre artistico",
+    name: "nombreartistico",
     label: "Ingrese su apodo:",
     value: inputValue.correoartistico
   });
@@ -145,7 +146,70 @@ function App() {
     label: "salario:",
     value: inputValue.salario
   });
+  const handleSubmit = (event) => {
 
+    let form = event.target
+
+    
+        
+    if (!form.checkValidity()){
+        event.preventDefault()
+        event.stopPropagation()
+    } else {
+        handleSubmitJsPdf()
+    }
+    form.classList.add('was-validated')
+  }
+
+  const handleChange = (e) => {
+    let {name , value} = e.target
+
+    setInputValue({...inputValue, [name]:value})
+  }
+
+  const handleSubmitJsPdf = () => {
+
+    const doc = new jsPDF();
+
+    let fecha="11-05-2022";
+    let nombrecarinosa = document.getElementById("nombrecarinosa").value;
+    let nombreartistico =document.getElementById("nombreartistico").value;
+
+    doc.setFillColor(28, 41, 51);
+
+    doc.rect(0, 0, 220, 60, 'f');
+
+    //doc.addImage(ImageData, 10, 8, 106, 34);
+
+    doc.setFontSize(14);
+
+    doc.setFontType("bold");
+
+    doc.setTextColor(22, 225, 255);
+
+    doc.text('Cariñosas premium.com  2023',12, 50);
+
+    let counter = 90
+
+    doc.setTextColor(37, 40, 80);
+
+    inputsProps.forEach((item) => {
+        let input = item.value
+        let label = item.label
+
+        doc.text(`${label} ${input}`, 12, counter);
+        counter = counter + 10;
+    })
+
+    let partes = fecha.split('-');
+
+    var fechadoc = partes[2]+ '-'+ partes[1]+ '-' + partes[0];
+
+    console.log("E");
+    const docpdf = `${nombrecarinosa} ${nombreartistico} ${fechadoc}.pdf`;
+    doc.save(docpdf);
+
+}
   return (
   <></>
   );
